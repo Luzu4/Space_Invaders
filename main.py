389a7a2a56e504e803bdcf5080322d4041e6c2c7
Luz <maciekluz@gmail.com>
2021-05-10 23:49:10 +0200

    without menu

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import turtle
 import time
 import random
-INVADERS_POS = [(50, 0), (100, 0), (150, 0),(0, 0), (-50, 0), (-100, 0), (-150, 0),
-                (50, 30), (100, 30), (150, 30),(0, 30), (-50, 30), (-100, 30), (-150, 30),
-                (50, 60), (100, 60), (150, 60), (0, 60), (-50, 60), (-100, 60), (-150, 60),
-                (50, 90), (100, 90), (150, 90), (0, 90), (-50, 90), (-100, 90), (-150, 90)
+from operator import itemgetter
+
+INVADERS_POS = [(-150, 90), (-150, 60), (-150, 30), (-150, 0),
+                (50, 0), (100, 0), (150, 0), (0, 0), (-50, 0), (-100, 0),
+                (50, 30), (100, 30), (150, 30), (0, 30), (-50, 30), (-100, 30),
+                (50, 60), (100, 60), (0, 60), (-50, 60), (-100, 60),
+                (50, 90), (100, 90), (0, 90), (-50, 90), (-100, 90), (150, 60), (150, 90)
                 ]
+# Sort list for
+INVADERS_POS.sort(key=itemgetter(0))
 
 
 class Ship(turtle.Turtle):
@@ -16,7 +21,7 @@ class Ship(turtle.Turtle):
         self.shapesize(3, 3, 3)
         self.penup()
         self.left(90)
-        self.setposition(0,-250)
+        self.setposition(0, -250)
 
 
 class Invaders(turtle.Turtle):
@@ -56,6 +61,8 @@ class Game:
         self.bullets = []
         self.invaders_bullets = []
         self.count = 0
+        self.heights = 0
+        self.count_heights = 0
         self.speed = 2
         self.plane = Ship()
         self.set_invaders()
@@ -67,10 +74,12 @@ class Game:
         self.window.mainloop()
 
     def move_right(self):
-        self.plane.goto(self.plane.xcor()+5, self.plane.ycor())
+        if self.plane.xcor() < 400:
+            self.plane.goto(self.plane.xcor() + 5, self.plane.ycor())
 
     def move_left(self):
-        self.plane.goto(self.plane.xcor()-5, self.plane.ycor())
+        if self.plane.xcor() > -400:
+            self.plane.goto(self.plane.xcor() - 5, self.plane.ycor())
 
     def set_invaders(self):
         for invader in INVADERS_POS:
@@ -93,33 +102,43 @@ class Game:
         self.__init__()
 
     def move(self):
-        if self.invaders_ships[0].xcor() > 200 or self.invaders_ships[0].xcor() < -200:
+
+        if self.invaders_ships[len(self.invaders_ships) - 1].xcor() >= 400 or self.invaders_ships[0].xcor() <= -400:
             self.speed *= -1
         for ship in self.invaders_ships:
-            ship.goto(ship.xcor() + self.speed, ship.ycor())
-        self.window.update()
-
+            if ship.ycor() <= self.plane.ycor():
+                self.new_game()
+            ship.goto(ship.xcor() + self.speed, ship.ycor() + self.heights)
         for invader_rocket in self.invaders_bullets:
             invader_rocket.goto(invader_rocket.xcor(), invader_rocket.ycor() - 5)
-            if self.plane.xcor() - 20 <= invader_rocket.xcor() <= self.plane.xcor() + 20 and\
-                self.plane.ycor() - 5 <= invader_rocket.ycor() <= self.plane.ycor() + 5:
+            if self.plane.xcor() - 20 <= invader_rocket.xcor() <= self.plane.xcor() + 20 and \
+                    self.plane.ycor() - 5 <= invader_rocket.ycor() <= self.plane.ycor() + 5:
                 print('You Lose!')
                 self.new_game()
         for rocket in self.bullets:
             rocket.goto(rocket.xcor(), rocket.ycor() + 5)
             for ship in self.invaders_ships:
-                if ship.xcor() - 10 <= rocket.xcor() <= ship.xcor() + 10 and\
+                if ship.xcor() - 10 <= rocket.xcor() <= ship.xcor() + 10 and \
                         ship.ycor() - 10 <= rocket.ycor() <= ship.ycor() + 10:
                     self.invaders_ships.remove(ship)
                     self.bullets.remove(rocket)
-                    rocket.color('black')
-                    ship.color('black')
+                    rocket.hideturtle()
+                    ship.hideturtle()
         self.count += 1
+        self.count_heights += 1
+        self.heights = 0
+        if len(self.invaders_ships) == 0:
+            self.new_game()
+        if self.count_heights == 500:
+            self.heights = -10
+            self.count_heights = 0
         if self.count == 50:
             random_number = random.randint(0, len(self.invaders_ships) - 1)
             self.invader_fire(self.invaders_ships[random_number].xcor(), self.invaders_ships[random_number].ycor())
             self.count = 0
-
+        self.window.update()
         time.sleep(0.01)
         self.window.ontimer(self.move)
+
+
 game = Game()
